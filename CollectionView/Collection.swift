1f5831f4191ebc673b2a834dddf2ac0f2b86de40
Kelvin <zhiyao92@go.byuh.edu>
2017-10-12 09:42:04 -1000

    First Commit

---
+++ CollectionView/Collection.swift
@@ -0,0 +1,19 @@
+//
+//  Collection.swift
+//  CollectionView
+//
+//  Created by Kelvin on 10/11/17.
+//  Copyright © 2017 Kelvin. All rights reserved.
+//
+
+import Foundation
+
+struct Collection {
+    var image: String = ""
+    var label: String = ""
+    
+    init(image: String, label: String) {
+        self.image = image
+        self.label = label
+    }
+}
---
+++ CollectionView/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Kelvin on 10/11/17.
+//  Copyright © 2017 Kelvin. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var collectionImage: UIImageView!
+    @IBOutlet weak var collectionLabel: UILabel!
+    
+}
